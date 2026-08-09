c81e8c5b0d05fac004878a0747cd8f64a73a5d21
Aun <aaunr5@gmail.com>
2026-08-08 23:49:27 -0400

    Add files via upload

--- port_optimizer.py
+++ port_optimizer.py
@@ -12,16 +12,39 @@ import math as math
 #show weights for each stock for the max sharpe 
 
 
-rng = np.random.default_rng()
-data = pd.DataFrame(yf.download('AAPL', period = '5y'))['Close'].pct_change()
-data["GOOGL"] = pd.DataFrame(yf.download('GOOGL', period = '5y'))['Close'].pct_change()
-data["TSLA"] = pd.DataFrame(yf.download('TSLA', period = '5y'))['Close'].pct_change()
-data["GM"] = pd.DataFrame(yf.download('GM', period = '5y'))['Close'].pct_change()
+data = pd.DataFrame()
+num_stock = 0
+
+while True:
+        stock = str(input("What stock do you want to analyze? (X to exit"))
+        if(stock.lower() == 'x'):
+            break
+        data[f"Stock{num_stock}"] = pd.DataFrame(yf.download(stock, period='1y'))["Close"].pct_change()
+        if(data[f"Stock{num_stock}"].empty):
+             print("Try again, please use the ticker symbol!")
+             continue 
+        num_stock += 1
+
+
+# rng = np.random.default_rng()
+# data = pd.DataFrame(yf.download('AAPL', period = '5y'))['Close'].pct_change()
+# data["GOOGL"] = pd.DataFrame(yf.download('GOOGL', period = '5y'))['Close'].pct_change()
+# data["TSLA"] = pd.DataFrame(yf.download('TSLA', period = '5y'))['Close'].pct_change()
+# data["GM"] = pd.DataFrame(yf.download('GM', period = '5y'))['Close'].pct_change()
 
 cov_annual = data.cov()*252
-stock_returns = np.array([data["AAPL"].mean() * 252, data["GOOGL"].mean() * 252, data["TSLA"].mean() * 252, data["GM"].mean() * 252])
+# stock_returns = np.array([data["AAPL"].mean() * 252, data["GOOGL"].mean() * 252, data["TSLA"].mean() * 252, data["GM"].mean() * 252])
+stock_returns = []
+
+
+for i in range(num_stock):
+     stock_returns.append(data[f"Stock{i}"].mean() * 252)
+
+stock_returns = np.array(stock_returns)
+
 sharpe_ratios = []
-print(cov_annual @ [0.2,0.3,0.4,0.5] @ [0.2,0.3,0.4,0.5] )
+
+
 
 vols = [] # risk 
 returns = [] # return
@@ -36,16 +59,23 @@ def f(x):
 
     
 
+bnds = []
+for i in range(num_stock):
+     bnds.append((0,1))
+
+guess = []
+for i in range(num_stock):
+     guess.append((0.25))
 
 
 
-max_sharpe = minimize(f,[0.25,0.25,0.25,0.25], bounds = ((0,1),(0,1),(0,1),(0,1)), constraints= {'type': 'eq', 'fun': lambda x: x.sum() - 1}, method = "SLSQP")
+max_sharpe = minimize(f,guess, bounds = bnds, constraints= {'type': 'eq', 'fun': lambda x: x.sum() - 1}, method = "SLSQP")
 max_sharpe_x = (np.sqrt(cov_annual @ max_sharpe['x'] @ max_sharpe['x']))
 max_sharpe_y = ((stock_returns * max_sharpe['x']).sum())
 
 
 for i in range(10000):
-    num = (np.random.uniform(low = 0, high = 1, size = 4))
+    num = (np.random.uniform(low = 0, high = 1, size = num_stock))
     sum = num.sum()
     num = num/sum
     returns.append((stock_returns * num).sum())
